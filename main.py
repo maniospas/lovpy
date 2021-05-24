d68cf404ae59eda50b75bb4c5ec8bb7c1ecd9c77
Dimitris Karageorgiou (University Repo) <soulrain@outlook.com>
2021-05-25 01:21:13 +0300

    Added valid thread test.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from logipy.visualization_strategies import set_full_visualization_scheme
 # from logipy.visualization_strategies import set_proving_process_visualization_enabled
 
 import examples.invalid_test
+# import examples.valid_thread_test
 # import examples.invalid_thread_test
 # import examples.numpy_test
 # import examples.scipy_test
